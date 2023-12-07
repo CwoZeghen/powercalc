04a1f2016b34d4b227cfca927bd21fafffb9090c
CwoZeghen <cwo@zeghen.com>
2023-12-07 12:03:48 +0200

    34567890765

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,12 +2,14 @@
 <div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-lg-3 col-md-6">
-                <h5 class="text-white mb-4">Address</h5>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
-                <div class="d-flex pt-2">
+            <div class="col-lg-6 col-md-6">
+                <h5 class="text-white mb-4">Contact</h5>
+                {{-- <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Lubum</p> --}}
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:+27 87-265-8624">+27 87-265-8624</a></p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:+243 89-834-2915">+243 89-834-2915</a>
+                </p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>support@zeghen.com</p>
+                {{-- <div class="d-flex pt-2">
                     <a class="btn btn-square btn-outline-light btn-social" href=""><i
                             class="fab fa-twitter"></i></a>
                     <a class="btn btn-square btn-outline-light btn-social" href=""><i
@@ -16,17 +18,14 @@
                             class="fab fa-youtube"></i></a>
                     <a class="btn btn-square btn-outline-light btn-social" href=""><i
                             class="fab fa-linkedin-in"></i></a>
-                </div>
+                </div> --}}
             </div>
-            <div class="col-lg-3 col-md-6">
+            <div class="col-lg-6 col-md-6">
                 <h5 class="text-white mb-4">Quick Links</h5>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Our Services</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
-                <a class="btn btn-link" href="">Support</a>
+                <a class="btn btn-link" href="{{ route('home') }}">Home</a>
+                <a class="btn btn-link" href="#power-calculator">Calculator</a>
             </div>
-            <div class="col-lg-3 col-md-6">
+            {{-- <div class="col-lg-3 col-md-6">
                 <h5 class="text-white mb-4">Project Gallery</h5>
                 <div class="row g-2">
                     <div class="col-4">
@@ -57,19 +56,19 @@
                     <button type="button"
                         class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
     <div class="container">
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                    &copy; {{ date('Y') }} <a href="#">{{ config('app.name') }}</a>, All Right Reserved.
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a href="https://htmlcodex.com">HTML Codex</a>
-                    <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    {{-- Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                    <br>Distributed By: <a href="https://themewagon.com" target="_blank">ThemeWagon</a> --}}
                 </div>
             </div>
         </div>
@@ -96,4 +95,3 @@
 
 <!-- Template Javascript -->
 <script src="{{ asset('/') }}js/main.js"></script>
-
